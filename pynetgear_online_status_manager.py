350c88f10cce23877807481d2629187624b2c84f
lamusmaser <lamusmaser@gmail.com>
2024-08-26 20:10:55 -0600

    Fix Chrome options to run in Linux.

--- pynetgear_online_status_manager.py
+++ pynetgear_online_status_manager.py
@@ -137,8 +137,10 @@ def main():
         )
 
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-gpu")
     options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(options=options)
